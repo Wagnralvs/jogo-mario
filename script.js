2499f325f2118add596e0c5be7588899905893dd
Wagner Alves Viana <walvesvi@emeal.nttdata.com>
2023-06-29 13:09:08 -0300

    criação da pontuação

--- script.js
+++ script.js
@@ -43,7 +43,6 @@ const loop = setInterval(() => {
     mario.style.bottom = `${positionMario}px`;
     mario.src = "./image/game-over.png";
     mario.style.width = "3.5rem";
-
     cloud.style.animation = "none";
     cloud.style.left = `${positionCloud}px`;
 
@@ -82,7 +81,6 @@ function cronom() {
       cloud.style.animation = "animacao_pipe 5.5s linear infinite";
       pipe.style.animation = "animacao_pipe 2s linear infinite";
       clearInterval(interval);
-      // looping();
       pointScore();
     }
     crono.innerHTML = ss;
@@ -92,8 +90,8 @@ function cronom() {
 function pointScore() {
   var points = 0;
   const score = setInterval(() => {
-  
-    if (mario.src == "http://127.0.0.1:5500/image/game-over.png") {
+
+    if (mario.style.animationName == "none" && pipe.style.animationName == "none") {debugger
       point.innerHTML = points; 
       clearInterval(score)
     } else {
@@ -105,21 +103,12 @@ function pointScore() {
 }
 keepGame()
 function keepGame(){
-  const positionPipe = +window.getComputedStyle(pipe).left.replace("px", "");
-  
+ 
   btn_keepOn.addEventListener("click", ()=>{
     start.style.display = "flex";
     btn_start.style.display = "flex";
     gameOver.style.display="none";
-    point.innerHTML = 0;
-    mario.src='http://127.0.0.1:5500/image/mario.gif'
-    mario.style.width = "5rem";
-    mario.style.left = '0px';
-    mario.style.bottom = "0";
-    pipe.style.right = "200px"; 
-    pipe.style.bottom = "0";
-    pipe.style.left = '100px';
-    window.location.reload();
+     window.location.reload();
    
 })
 }
